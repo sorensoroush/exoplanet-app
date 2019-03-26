9338696afd399a02766dc5236b244c98d205d408
icelydone <soren03@hotmail.com>
2019-03-26 15:28:39 -0400

    Finally fixed orbits; todo: add more local planet orbits

--- src/components/RandomPlanet.js
+++ src/components/RandomPlanet.js
@@ -1,49 +1,58 @@
 import React, { Component } from 'react'
 
-let canvas, ctx, plEccen, plMaxOrb, randPlanet
+let canvas, ctx, plEccen, plOrbAU, randPlanet
 const earthEccen = 0.0167
   
 class RandomPlanet extends Component {
   drawOrbits() {
     plEccen = randPlanet.pl_orbeccen
-    plMaxOrb = randPlanet.pl_orbsmax
-    const plOrbRad = plMaxOrb * 145
-    console.log(plEccen, plMaxOrb)
-    canvas = document.getElementById('orbits')
-    ctx = canvas.getContext('2d')
+    plOrbAU = randPlanet.pl_orbsmax
+    const plMajRad = plOrbAU * 145
+    console.log(plEccen, plOrbAU)
     ctx.lineWidth = 2
     ctx.strokeStyle = '#000000'
     ctx.strokeText('+', 147, 153)
-    if (plMaxOrb) {
-      ctx.beginPath()
-      // if (plEccen !== null) {
-        // const plFocusLen = plOrbRad * plEccen
-        // const plMajAxis = plOrbRad - plFocusLen
-        // ctx.ellipse(150 + plFocusLen, 150, 150 * plMajAxis, Math.pow(
-      if (plMaxOrb < 1) {
-        ctx.ellipse(150, 150, plOrbRad, plOrbRad, 0, 0, 2 * Math.PI)
+
+    ctx.beginPath()
+    if (plEccen !== null) {
+      const plFocusLen = plMajRad * plEccen
+      const plMinRad = Math.sqrt(Math.pow(plMajRad,2) - Math.pow(plFocusLen,2))
+      if (plOrbAU < 1) {
+        ctx.ellipse(150 + plFocusLen, 150, plMajRad, plMinRad, 0, 0, 2 * Math.PI)
+      } else {
+        ctx.ellipse(150 + plFocusLen / plOrbAU, 150, 145, 145 * (plMinRad/plMajRad), 0, 0, 2 * Math.PI)
+      }
+      ctx.stroke()
+    } else {
+      if (plOrbAU < 1) {
+        ctx.ellipse(150, 150, plMajRad, plMajRad, 0, 0, 2 * Math.PI)
       } else {
         ctx.ellipse(150, 150, 145, 145, 0, 0, 2 * Math.PI)
       }
       ctx.stroke()
     }
+
     ctx.beginPath()
-    if (plMaxOrb < 1) {
+    if (plOrbAU < 1) {
       ctx.ellipse(150, 150, 145, 145, 0, 0, 2 * Math.PI)
     } else {
-      ctx.ellipse(150, 150, (1/plMaxOrb) * 145, (1/plMaxOrb) * 145, 0, 0, 2 * Math.PI)
+      ctx.ellipse(150, 150, (1/plOrbAU) * 145, (1/plOrbAU) * 145, 0, 0, 2 * Math.PI)
     }
     ctx.strokeStyle = '#0000FF'
     ctx.stroke()
   }
     
   componentDidMount() {
-    this.drawOrbits()
+    canvas = document.getElementById('orbits')
+    ctx = canvas.getContext('2d')
+    if (randPlanet.pl_orbsmax) {this.drawOrbits()}
   }
 
   componentDidUpdate() {
     ctx.clearRect(0, 0, 300, 300)
-    this.drawOrbits()
+    if (randPlanet.pl_orbsmax) {
+      this.drawOrbits()
+    }
   }
    
   render () {
@@ -65,10 +74,27 @@ class RandomPlanet extends Component {
             <canvas id="orbits" width="300" height="300"></canvas>
           </div>
           <div id="orbits-info">
-            <p style={{color: 'blue'}}>Earth orbit</p>
-            <p>{randPlanet.pl_name} orbit</p>
-            {randPlanet.pl_orbeccen !== null ? <p>Eccentricity: {randPlanet.pl_orbeccen}</p> : null}
-            {randPlanet.pl_orbsmax !== null ? <p>Max orbit radius: {randPlanet.pl_orbsmax} AU</p> : <p>Orbit information unknown</p>}
+            {randPlanet.pl_orbsmax !== null ? 
+              <div>
+              <p style={{color: 'blue', textDecoration: 'underline'}}>Earth's orbit</p>
+              <p style={{color: 'blue'}}>Eccentricity: {earthEccen}</p>
+              <p style={{color: 'blue'}}>Semimajor axis radius: 1 AU</p>
+              <p style={{color: 'blue'}}>Orbit period: 365 days</p>
+              <br />
+              <p style={{textDecoration: 'underline'}}>{randPlanet.pl_name}'s orbit</p>
+              {randPlanet.pl_orbeccen !== null ? <p>Eccentricity: {randPlanet.pl_orbeccen}</p> : null}
+              <p>Semimajor axis radius: {randPlanet.pl_orbsmax} AU</p>
+              {randPlanet.pl_orbper !== null ? <p>Orbit period: {Math.round(randPlanet.pl_orbper)} days</p> : null}
+              </div>
+            : 
+              randPlanet.pl_orbeccen !== null ?
+                <p>Database error: {randPlanet.pl_name}'s orbit eccentricity is {randPlanet.pl_orbeccen}, yet semimajor axis radius is not listed. Find it {'here'}.</p>
+              : 
+                <p>Orbit information unknown</p>
+              }
+          </div>
+          <div id="planets-canvas">
+            <canvas id="sizes" width="300" height="300"></canvas>
           </div>
         </div>
         <ul>
